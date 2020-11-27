575feb2bc82203cb2d9010d5a6e6e3bce518a6cc
Damian Pfammatter <pfammatterdamian@gmail.com>
2020-11-27 10:37:39 +0100

    Update advisories

--- app/api/advisory.py
+++ app/api/advisory.py
@@ -9,8 +9,9 @@ from app.schemas.csaf import CSAFv2Schema
 @bp.route('/advisory', methods=['POST'])
 @validate_schema(CSAFv2Schema)
 def create_advisory():
-    # Create and save new advisory
+    # Load data
     data = request.get_json() or {}
+    # Create and save new advisory
     advisory = Advisory(**data)
     advisory.save()
     # Return response
@@ -34,7 +35,19 @@ def export_advisory(uid):
     return get_advisory(uid, include_metadata=False)
 
 
-##@bp.route('/advisory/<int:uid>', methods=['PUT'])
-##@validate_schema(CSAFv2Schema)
-##def update_advisory(uid):
-##    pass
+@bp.route('/advisory/<int:uid>', methods=['PUT'])
+@validate_schema(CSAFv2Schema)
+def update_advisory(uid):
+    # Load data
+    data = request.get_json() or {}
+    # Update and save existing advisory
+    advisory = Advisory.objects(_id=uid).first()
+    if advisory is None: abort(404, 'Advisory not found.')
+    advisory.modify(**data)
+    advisory.save()
+    # Return response
+    response = jsonify(advisory.to_json())
+    response.status_code = 200
+    response.headers['Location'] = url_for('api.get_advisory', uid=advisory._id)
+    return response
+    
